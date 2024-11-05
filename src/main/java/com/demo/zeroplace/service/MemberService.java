990b79ec0d7920fc4edad00af4f63752624baeef
hye-inA <hye.ina0130@gmail.com>
2024-11-05 21:49:07 +0900

    트랜잭션 적용

--- src/main/java/com/demo/zeroplace/service/MemberService.java
+++ src/main/java/com/demo/zeroplace/service/MemberService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.demo.zeroplace.dto.request.MemberCreateRequest;
 import com.demo.zeroplace.dto.request.MemberUpdateRequest;
 import com.demo.zeroplace.dto.response.MemberResponse;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,23 +21,24 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
+    @Transactional
     public void saveMember(MemberCreateRequest request) {
         memberRepository.save(new Member(request.getName(), request.getTel()));
     }
-
+    @Transactional
     public void updateMember(MemberUpdateRequest request) {
         Member member = memberRepository.findById(request.getId())
                 .orElseThrow(IllegalArgumentException::new);
         member.updateTel(request.getTel());
         memberRepository.save(member);
     }
-
+    @Transactional(readOnly = true)
     public List<MemberResponse> getMember() {
         return memberRepository.findAll().stream()
                 .map(member -> new MemberResponse(member.getId(), member.getName(), member.getTel()))
                 .collect(Collectors.toList());
     }
-
+    @Transactional
     public void deleteMember(String tel) {
         Member member = memberRepository.findByTel(tel);
         if (member == null) {
