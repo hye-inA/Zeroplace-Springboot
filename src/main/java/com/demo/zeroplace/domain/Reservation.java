362285e566ceba1cb3eb9f34595c5677a6bac47c
hye-inA <hye.ina0130@gmail.com>
2024-11-12 14:42:24 +0900

    reservation 엔티티 생성

---
+++ src/main/java/com/demo/zeroplace/domain/Reservation.java
@@ -0,0 +1,42 @@
+package com.demo.zeroplace.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(force = true,access = AccessLevel.PROTECTED)
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer numOfPeople;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "studyroom_id")
+    private Studyroom studyroom;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    @Builder
+    public Reservation(Long id, Integer numOfPeople, Member member, Studyroom studyroom, LocalDateTime startTime, LocalDateTime endTime) {
+        this.id = id;
+        this.numOfPeople = numOfPeople;
+        this.member = member;
+        this.studyroom = studyroom;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
