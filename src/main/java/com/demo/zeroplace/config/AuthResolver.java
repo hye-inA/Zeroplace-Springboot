941f41b9b376f3080a143c7654c14ec90eb48fce
hye-inA <hye.ina0130@gmail.com>
2024-11-18 00:02:13 +0900

    jwt 토큰으로 암호화

--- src/main/java/com/demo/zeroplace/config/AuthResolver.java
+++ src/main/java/com/demo/zeroplace/config/AuthResolver.java
@@ -1,36 +1,53 @@
 package com.demo.zeroplace.config;
 
-import com.demo.zeroplace.config.data.UserSession;
-import com.demo.zeroplace.domain.Session;
+import com.demo.zeroplace.config.data.MemberSession;
 import com.demo.zeroplace.exception.Unauthorized;
 import com.demo.zeroplace.repository.SessionRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import java.util.Base64;
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
     private final SessionRepository sessionRepository;
+    private static final String KEY = ;
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.getParameterType().equals(UserSession.class);
+        return parameter.getParameterType().equals(MemberSession.class);
     }
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory)  {
-        String accessToken = webRequest.getHeader("Authorization");
+        String jws = webRequest.getHeader("Authorization");
 
-        if (accessToken == null || accessToken.equals("")){
+        if (jws == null || jws.equals("")){
+            throw new Unauthorized();
+        }
+        byte[] decodeKey = Base64.getDecoder().decode(KEY);
+
+        // 복호화
+        try {
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(decodeKey)
+                    .build()
+                    .parseSignedClaims(jws);
+            String memberId = claims.getBody().getSubject();
+            return new MemberSession(Long.parseLong(memberId));
+
+        } catch (JwtException e) {
             throw new Unauthorized();
         }
 
-        Session session = sessionRepository.findByAccessToken(accessToken)
-                .orElseThrow(Unauthorized::new);
-
-        return new UserSession(session.getMember().getId());
     }
 }
--- src/main/java/com/demo/zeroplace/controller/AuthController.java
+++ src/main/java/com/demo/zeroplace/controller/AuthController.java
@@ -3,11 +3,16 @@ package com.demo.zeroplace.controller;
 import com.demo.zeroplace.dto.request.Login;
 import com.demo.zeroplace.dto.response.SessionResponse;
 import com.demo.zeroplace.service.AuthService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
 
 @Slf4j
 @RestController
@@ -15,10 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    private final String KEY = ;
     @PostMapping("/auth/login")
     public SessionResponse login(@RequestBody Login login) {
+        Long memberId = authService.signin(login);
 
-        String accessToken = authService.signin(login);
-        return new SessionResponse(accessToken);
+        SecretKey key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(KEY));
+
+        String jws = Jwts.builder()
+                .subject(String.valueOf(memberId))
+                .signWith(key)
+                .compact();
+
+        return new SessionResponse(jws);
     }
 }
